b14bf74e98e33d3125493cefef17b3500f714863
Kaketo Shimizu <ipkake0527@yahoo.co.jp>
2023-03-16 13:38:09 +0900

    [quickstart]data to tensor

--- quickStart.py
+++ quickStart.py
@@ -20,3 +20,16 @@ test_data = datasets.FashionMNIST(
     download=True,
     transform=ToTensor(),
 )
+
+batch_size = 64
+
+# Create data loaders.
+train_dataloader = DataLoader(training_data, batch_size=batch_size)
+test_dataloader = DataLoader(test_data, batch_size=batch_size)
+
+for X, y in test_dataloader:
+    print(f"Shape of X [N, C, H, W]: {X.shape}")
+    print(f"Shape of y: {y.shape} {y.dtype}")
+    print(f"Type of X: {type(X)}")
+    print(f"Type of y: {type(y)}")
+    break
