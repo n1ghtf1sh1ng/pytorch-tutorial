616084fa04c7bcd5449adf677bcd711db2ddbe8a
Kaketo Shimizu <ipkake0527@yahoo.co.jp>
2023-03-17 15:26:28 +0900

    [quickstart] loading models

--- quickStart.py
+++ quickStart.py
@@ -109,14 +109,39 @@ def test(dataloader, model, loss_fn):
     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
 
-# The training process is conducted over several iterations (epochs). During each epoch, the model learns parameters to make better predictions. We print the model’s accuracy and loss at each epoch; we’d like to see the accuracy increase and the loss decrease with every epoch.
+# the training process is conducted over several iterations (epochs). during each epoch, the model learns parameters to make better predictions. we print the model’s accuracy and loss at each epoch; we’d like to see the accuracy increase and the loss decrease with every epoch.
 epochs = 5
 for t in range(epochs):
-    print(f"Epoch {t+1}\n-------------------------------")
+    print(f"epoch {t+1}\n-------------------------------")
     train(train_dataloader, model, loss_fn, optimizer)
     test(test_dataloader, model, loss_fn)
-print("Done!")
+print("done!")
 
-# A common way to save a model is to serialize the internal state dictionary (containing the model parameters).
+# a common way to save a model is to serialize the internal state dictionary (containing the model parameters).
 torch.save(model.state_dict(), "model.pth")
-print("Saved PyTorch Model State to model.pth")
+print("saved pytorch model state to model.pth")
+
+# The process for loading a model includes re-creating the model structure and loading the state dictionary into it.
+model = NeuralNetwork()
+model.load_state_dict(torch.load("model.pth"))
+
+# This model can now be used to make predictions.
+classes = [
+    "T-shirt/top",
+    "Trouser",
+    "Pullover",
+    "Dress",
+    "Coat",
+    "Sandal",
+    "Shirt",
+    "Sneaker",
+    "Bag",
+    "Ankle boot",
+]
+
+model.eval()
+x, y = test_data[0][0], test_data[0][1]
+with torch.no_grad():
+    pred = model(x)
+    predicted, actual = classes[pred[0].argmax(0)], classes[y]
+    print(f'Predicted: "{predicted}", Actual: "{actual}"')
