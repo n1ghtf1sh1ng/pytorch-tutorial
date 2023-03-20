effd29f53a8e0996bf24c4bb2aaf2d922d84251f
Kaketo Shimizu <ipkake0527@yahoo.co.jp>
2023-03-20 15:12:53 +0900

    [tensors] attributes of a tensor

--- tensors.py
+++ tensors.py
@@ -27,3 +27,11 @@ zeros_tensor = torch.zeros(shape)
 print(f"Random Tensor: \n {rand_tensor} \n")
 print(f"Ones Tensor: \n {ones_tensor} \n")
 print(f"Zeros Tensor: \n {zeros_tensor}")
+
+
+# Tensor attributes describe their shape, datatype, and the device on which they are stored.
+tensor = torch.rand(3, 4)
+
+print(f"Shape of tensor: {tensor.shape}")
+print(f"Datatype of tensor: {tensor.dtype}")
+print(f"Device tensor is stored on: {tensor.device}")
