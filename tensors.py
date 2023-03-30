bb1d58d011a5147f884aa385587c84736fec1d04
Kaketo Shimizu <ipkake0527@yahoo.co.jp>
2023-03-30 10:48:07 +0900

    [tensors] operations on tensors

--- tensors.py
+++ tensors.py
@@ -4,19 +4,19 @@ import numpy as np
 # Tensors can be created directly from data. The data type is automatically inferred.
 data = [[1, 2], [3, 4]]
 x_data = torch.tensor(data)
-print(f'x_data: {x_data}\n')
+# print(f'x_data: {x_data}\n')
 
 # Tensors can be created from NumPy arrays (and vice versa - see Bridge with NumPy).
 np_array = np.array(data)
 x_np = torch.from_numpy(np_array)
-print(f'x_np: {x_np}\n')
+# print(f'x_np: {x_np}\n')
 
 # The new tensor retains the properties (shape, datatype) of the argument tensor, unless explicitly overridden.
 x_ones = torch.ones_like(x_data)  # retains the properties of x_data
-print(f"Ones Tensor: \n {x_ones} \n")
+# print(f"Ones Tensor: \n {x_ones} \n")
 
 x_rand = torch.rand_like(x_data, dtype=torch.float)  # overrides the datatype of x_data
-print(f"Random Tensor: \n {x_rand} \n")
+# print(f"Random Tensor: \n {x_rand} \n")
 
 # shape is a tuple of tensor dimensions. In the functions below, it determines the dimensionality of the output tensor.
 shape = (2, 3,)
@@ -24,14 +24,65 @@ rand_tensor = torch.rand(shape)
 ones_tensor = torch.ones(shape)
 zeros_tensor = torch.zeros(shape)
 
-print(f"Random Tensor: \n {rand_tensor} \n")
-print(f"Ones Tensor: \n {ones_tensor} \n")
-print(f"Zeros Tensor: \n {zeros_tensor}")
+# print(f"Random Tensor: \n {rand_tensor} \n")
+# print(f"Ones Tensor: \n {ones_tensor} \n")
+# print(f"Zeros Tensor: \n {zeros_tensor}")
 
 
 # Tensor attributes describe their shape, datatype, and the device on which they are stored.
 tensor = torch.rand(3, 4)
 
-print(f"Shape of tensor: {tensor.shape}")
-print(f"Datatype of tensor: {tensor.dtype}")
-print(f"Device tensor is stored on: {tensor.device}")
+# print(f"Shape of tensor: {tensor.shape}")
+# print(f"Datatype of tensor: {tensor.dtype}")
+# print(f"Device tensor is stored on: {tensor.device}")
+
+
+# Over 100 tensor operations, including arithmetic, linear algebra, matrix manipulation (transposing, indexing, slicing), sampling and more are comprehensively described here.
+
+# Each of these operations can be run on the GPU(at typically higher speeds than on a CPU). If you’re using Colab, allocate a GPU by going to Runtime > Change runtime type > GPU.
+
+# By default, tensors are created on the CPU. We need to explicitly move tensors to the GPU using .to method(after checking for GPU availability). Keep in mind that copying large tensors across devices can be expensive in terms of time and memory!
+
+# We move our tensor to the GPU if available
+if torch.cuda.is_available():
+    tensor = tensor.to("cuda")
+
+# Standard numpy-like indexing and slicing:
+tensor = torch.ones(4, 4)
+# print(f"First row: {tensor[0]}")
+# print(f"First column: {tensor[:, 0]}")
+# print(f"Last column: {tensor[..., -1]}")
+tensor[:, 1] = 0
+# print(tensor)
+
+# You can use torch.cat to concatenate a sequence of tensors along a given dimension. See also torch.stack, another tensor joining option that is subtly different from torch.cat.
+t1 = torch.cat([tensor, tensor, tensor], dim=1)
+# print(t1)
+
+# This computes the matrix multiplication between two tensors. y1, y2, y3 will have the same value
+# ``tensor.T`` returns the transpose of a tensor
+# matmul = matrix multiplication
+y1 = tensor @ tensor.T
+y2 = tensor.matmul(tensor.T)
+
+y3 = torch.rand_like(y1)
+torch.matmul(tensor, tensor.T, out=y3)
+# print(y1)
+
+# This computes the element-wise product. z1, z2, z3 will have the same value
+z1 = tensor * tensor
+z2 = tensor.mul(tensor)
+
+z3 = torch.rand_like(tensor)
+torch.mul(tensor, tensor, out=z3)
+# print(z1)
+
+# If you have a one-element tensor, for example by aggregating all values of a tensor into one value, you can convert it to a Python numerical value using item()
+agg = tensor.sum()
+agg_item = agg.item()
+# print(agg_item)
+
+# Operations that store the result into the operand are called in-place. They are denoted by a _ suffix. For example: x.copy_(y), x.t_(), will change x.
+print(f"{tensor} \n")
+tensor.add_(5)
+print(tensor)
