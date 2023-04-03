269185b839d7c7e29bc90d822066398e875592f5
Kaketo Shimizu <ipkake0527@yahoo.co.jp>
2023-04-03 15:28:15 +0900

    [transform] lambda

--- transforms.py
+++ transforms.py
@@ -9,3 +9,6 @@ ds = datasets.FashionMNIST(
     transform=ToTensor(),
     target_transform=Lambda(lambda y: torch.zeros(10, dtype=torch.float).scatter_(0, torch.tensor(y), value=1))
 )
+
+target_transform = Lambda(lambda y: torch.zeros(
+    10, dtype=torch.float).scatter_(dim=0, index=torch.tensor(y), value=1))
