11d05e4d589b651b79433dee8c25cd35251cb77c
Kaketo Shimizu <ipkake0527@yahoo.co.jp>
2023-04-04 10:49:46 +0900

    [fix] another way

--- differentiation.py
+++ differentiation.py
@@ -23,3 +23,8 @@ print(z.requires_grad)
 with torch.no_grad():
     z = torch.matmul(x, w) + b
 print(z.requires_grad)
+
+# another way
+z = torch.matmul(x, w) + b
+z_det = z.detach()
+print(z_det.requires_grad)
